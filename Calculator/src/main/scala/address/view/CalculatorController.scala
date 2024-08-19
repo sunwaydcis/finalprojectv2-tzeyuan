459777d4197ba245181e02469da913adcec1046f
tzeyuan <bubblesshu@gmail.com>
2024-08-19 20:16:41 +0800

    Update code of CalculatoController.scala

--- Calculator/src/main/scala/address/view/CalculatorController.scala
+++
@@ -1,5 +0,0 @@
-package address.view
-
-class CalculatorController {
-
-}
---
+++ Calculator/src/main/scala/ch/makery/address/view/CalculatorController.scala
@@ -0,0 +1,45 @@
+package ch.makery.address.view
+
+import scalafx.event.ActionEvent
+import scalafx.scene.control.{Button, TextField}
+import scalafxml.core.macros.sfxml
+import ch.makery.address.model.CalculatorModel
+
+@sfxml
+class CalculatorController(
+                            private val display: TextField,
+                            private val btn0: Button,
+                            private val btn1: Button,
+                            private val btn2: Button,
+                            private val btn3: Button,
+                            private val btn4: Button,
+                            private val btn5: Button,
+                            private val btn6: Button,
+                            private val btn7: Button,
+                            private val btn8: Button,
+                            private val btn9: Button,
+                            private val btnAdd: Button,
+                            private val btnMinus: Button,
+                            private val btnMultiply: Button,
+                            private val btnDivide: Button,
+                            private val btnEqual: Button,
+                            private val btnClear: Button
+                          ) {
+  private val model = new CalculatorModel()
+
+  // Function to handle button clicks
+  def handleButtonAction(event: ActionEvent): Unit = {
+    val buttonText = event.getSource.asInstanceOf[Button].text.value
+    buttonText match {
+      case "CL" => model.clear()
+      case "="  => model.calculate()
+      case _    => model.appendInput(buttonText)
+    }
+    updateDisplay()
+  }
+
+  // Function to update the display field
+  private def updateDisplay(): Unit = {
+    display.text = model.getCurrentResult
+  }
+}
