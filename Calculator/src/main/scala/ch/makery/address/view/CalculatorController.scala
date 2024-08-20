f313d1321f18ab76f439917ce77220b95656e6c0
tzeyuan <bubblesshu@gmail.com>
2024-08-20 13:48:12 +0800

    Modify the code of CalculatorController to solve the errors

--- Calculator/src/main/scala/ch/makery/address/view/CalculatorController.scala
+++ Calculator/src/main/scala/ch/makery/address/view/CalculatorController.scala
@@ -1,35 +1,36 @@
 package ch.makery.address.view
 
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, TextField}
+import scalafx.scene.control.TextField
 import scalafxml.core.macros.sfxml
 import ch.makery.address.model.CalculatorModel
+import javafx.scene.control.{Button => jfxButton}
 
 @sfxml
 class CalculatorController(
                             private val display: TextField,
-                            private val btn0: Button,
-                            private val btn1: Button,
-                            private val btn2: Button,
-                            private val btn3: Button,
-                            private val btn4: Button,
-                            private val btn5: Button,
-                            private val btn6: Button,
-                            private val btn7: Button,
-                            private val btn8: Button,
-                            private val btn9: Button,
-                            private val btnPlus: Button,
-                            private val btnMinus: Button,
-                            private val btnMultiply: Button,
-                            private val btnDivide: Button,
-                            private val btnEqual: Button,
-                            private val btnClear: Button
+                            private val btn0: jfxButton,
+                            private val btn1: jfxButton,
+                            private val btn2: jfxButton,
+                            private val btn3: jfxButton,
+                            private val btn4: jfxButton,
+                            private val btn5: jfxButton,
+                            private val btn6: jfxButton,
+                            private val btn7: jfxButton,
+                            private val btn8: jfxButton,
+                            private val btn9: jfxButton,
+                            private val btnPlus: jfxButton,
+                            private val btnMinus: jfxButton,
+                            private val btnMultiply: jfxButton,
+                            private val btnDivide: jfxButton,
+                            private val btnEqual: jfxButton,
+                            private val btnClear: jfxButton
                           ) {
   private val model = new CalculatorModel()
 
   // Function to handle button clicks
   def handleButtonAction(event: ActionEvent): Unit = {
-    val buttonText = event.getSource.asInstanceOf[Button].text.value
+    val buttonText = event.getSource.asInstanceOf[jfxButton].getText
     buttonText match {
       case "CL" => model.clear()
       case "="  => model.calculate()
