cd90e92c72fc90e80f2a8a26be803638d7559c98
tzeyuan <bubblesshu@gmail.com>
2024-08-20 13:34:06 +0800

    Update code to fix errors

--- Calculator/src/main/scala/ch/makery/address/MainCalculator.scala
+++ Calculator/src/main/scala/ch/makery/address/MainCalculator.scala
@@ -2,17 +2,22 @@ package ch.makery.address
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.scene.Scene
+import scalafx.scene.{Parent, Scene}
 import scalafxml.core.{FXMLLoader, FXMLView, NoDependencyResolver}
 import javafx.{scene => jfxs}
 
 object MainCalculator extends JFXApp {
 
-  val resource = getClass.getResource("/ch/makery/address/view/Calculator.fxml")
-  val root = FXMLView(resource, NoDependencyResolver)
+  val rootResource = getClass.getResource("view/Calculator.fxml")
+  val loader = new FXMLLoader(rootResource, NoDependencyResolver)
+  loader.load()
+  val roots = loader.getRoot[jfxs.Parent]
+  val scalaRoot: Parent = new Parent(roots){}
 
   stage = new PrimaryStage {
     title = "Calculator"
-    scene = new Scene(root)
+    scene = new Scene{
+        root = scalaRoot
+    }
   }
 }
